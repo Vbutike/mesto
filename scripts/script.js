5066a7b3d5057111ce613d642951b16c2fe8fba2
Andrey Beloedov <udaret34@yandex.ru>
2021-10-17 01:52:32 +0500

    Добавил Функционал

---
+++ scripts/script.js
@@ -0,0 +1,167 @@
+const windowEditProfil = document.querySelector('.popup_type_edit');
+const editButton = document.querySelector('.profile__edit-button');
+const nameInput = windowEditProfil.querySelector('.popup__input-text_type_name');
+const commentInput = windowEditProfil.querySelector('.popup__input-text_type_comment');
+const nameProfile = document.querySelector('.profile__name');
+const comentProfile = document.querySelector('.profile__coment');
+const form = windowEditProfil.querySelector('.popup__form');
+const modalWindowCloseBtn = document.querySelector('.popup__close-editprofil');
+const windowAddCard = document.querySelector('.popup_type_new-card');
+const cardWindowCloseBtn = windowAddCard.querySelector('.popup__close-card');
+const addButton = document.querySelector('.profile__add-button');
+const formAddCard = windowAddCard.querySelector('.popup__form');
+const nameInputMesto = windowAddCard.querySelector('.popup__input-text_type_namemesto');
+const inputUrl = windowAddCard.querySelector('.popup__input-text_type_url');
+const cards = document.querySelector('.elements');
+const templateElement = document.querySelector('#element-template').content;
+const windowPicture = document.querySelector('.popup_type_picture')
+const pictureWindowCloseBtn = windowPicture.querySelector('.popup__close-picture');
+const pictureOpen = windowPicture.querySelector('.popup__picture')
+const pictureOpenTitle = windowPicture.querySelector('.popup__title-picture')
+///////////////////////////////// СЛУШАТЕЛИ //////////////////////////////////////////
+//слушатели на клик мимо попапа
+windowEditProfil.addEventListener('click', onOverlayClick);
+windowAddCard.addEventListener('click', onOverlayClick);
+windowPicture.addEventListener('click', onOverlayClick);
+// Добавление новых карточек
+formAddCard.addEventListener('submit', createNewCard);
+
+// слушатель на нажатие кнопки 'Сохранить'
+form.addEventListener('submit',exchange);
+
+//слушаем нажатие на конпку для открытия попапов 
+editButton.addEventListener('click', openWindowEditProfil);
+addButton.addEventListener('click', openwindowAddCard);
+// Слушатели на кнопку закрытия попапов
+
+pictureWindowCloseBtn.addEventListener('click',()=>  closePopup(windowPicture));
+modalWindowCloseBtn.addEventListener('click',()=>  closePopup(windowEditProfil));
+cardWindowCloseBtn.addEventListener('click',()=> closePopup(windowAddCard));
+
+// Добавление новых карточек
+formAddCard.addEventListener('submit', createNewCard);
+
+//////////////////////////////////////////////////////////////////////////////////////
+//Функция открытия попапов
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+//Функция закрытия попапов
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//добавляем в форму действующие значения(имя и коментарий) и открываем попап  Редактирование профиля.
+
+function openWindowEditProfil() {
+    nameInput.value = nameProfile.textContent;
+    commentInput.value = comentProfile.textContent;
+    openPopup(windowEditProfil);
+}
+
+// Функция добавления на странницу инфо из попапа Редкатирования профиля
+function exchange (e) {
+  e.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  comentProfile.textContent = commentInput.value;
+  closePopup(windowEditProfil);
+}
+
+//Очищаем и открываем попап Добавления карточки
+function openwindowAddCard() {
+  nameInputMesto.value="";
+  inputUrl.value="";
+  openPopup(windowAddCard);
+ }
+
+////////Закрытие Popup если клик по странице////////////////////////////////////
+function onOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+      closePopup(windowEditProfil);
+      closePopup(windowAddCard);
+      closePopup(windowPicture);
+    };
+};
+//////////////////////////////////////////////////////////////////////////
+
+// Масив изначальных Карточек
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+  
+ // Функция добавления в начало уже созданной карточки на страницу
+function addCard(whereAdd, templateElement) {
+  whereAdd.prepend(templateElement);
+};
+
+// Функция создания карточки
+function createCard(titleCard, linkCardImg, templateElement) {
+  templateElement.querySelector('.element__title').textContent = titleCard;
+  templateElement.querySelector('.element__image').src = linkCardImg;
+  templateElement.querySelector('.element__image').alt = titleCard;
+
+//Лайк
+const likeBtn = templateElement.querySelector('.element__like-button');
+likeBtn.addEventListener('click', function(evt) {
+const eventTarget = evt.target;
+eventTarget.classList.toggle('element__like-button_active');
+});
+
+ // Функция удаления карточки
+ function delCard() {
+  templateElement.remove();
+};
+
+//Ставим слушатель на картинку в карточке, после клика запускаем функцию открытия попапа с картинкой.
+templateElement.querySelector('.element__image').addEventListener('click', ()=> {
+  openPopup(windowPicture);
+  pictureOpenTitle.alt = templateElement.querySelector('.element__title').textContent;
+  pictureOpenTitle.textContent = templateElement.querySelector('.element__title').textContent;
+  pictureOpen.src = templateElement.querySelector('.element__image').src;
+  });
+
+//Находим кнопку удаление карты и ставим слушатель
+const delBtn = templateElement.querySelector('.element__trash-btn');
+delBtn.addEventListener('click', delCard);
+};
+
+// из готового масива initialCards формируем и добавляем на страницу массив карточек.
+initialCards.forEach(function(item) {
+  const cardItem = templateElement.querySelector('.element').cloneNode(true);
+  createCard(item.name, item.link, cardItem);
+  addCard(cards, cardItem);
+});
+
+// Формируем новую карточку входные данные из попапа добавления карточки и добавляем ее на страницу
+function createNewCard (evt) {
+  evt.preventDefault();
+  const cardItem = templateElement.querySelector('.element').cloneNode(true);
+  createCard(nameInputMesto.value, inputUrl.value, cardItem);
+  addCard(cards, cardItem);
+  closePopup(windowAddCard);
+};
