4b75d940d412b77de6462eb2e5df927e29ff5606
Andrey Beloedov <udaret34@yandex.ru>
2021-11-22 23:26:34 +0500

    7.4

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -27,8 +27,8 @@ export class FormValidator {
         this.toggleButtonState();
     }
     
-    _hasInvalidInput(inputList) {
-        return inputList.some((inputElement) => {
+    _hasInvalidInput() {
+        return this._inputList.some((inputElement) => {
             return !inputElement.validity.valid;
         });
     }
